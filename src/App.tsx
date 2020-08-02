9973f5f1a40cd03a57129b1eea8d0f6d6c8ee481
Lucas <lucasvalenteds@gmail.com>
2020-08-02 20:22:15 -0300

    Replacing fragment syntax

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Native from 'react-native';
 
 export const App: React.FC = (): React.ReactElement => {
   return (
-    <>
+    <React.Fragment>
       <Native.StatusBar />
       <Native.SafeAreaView>
         <Native.ScrollView></Native.ScrollView>
       </Native.SafeAreaView>
-    </>
+    </React.Fragment>
   );
 };
